0fe5882314a7211fee4fb63c27f8af8b4bcbedc1
Bhargava-nani <matchabhargava07@gmail.com>
2026-05-07 13:29:17 +0530

    Fix typo in bot activity name

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Done with Heaven' God -- 𝐍𝐀𝐍𝐈 🥀",
+        name: "Done with Heaven's  God -- 𝐍𝐀𝐍𝐈 🥀",
         // Activity type number (0 = Playing).
         type: 0, 
       },
