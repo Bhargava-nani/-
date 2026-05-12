9643abb57e508de60e67ad778cf0df3b61c46ad8
Bhargava-nani <matchabhargava07@gmail.com>
2026-05-12 13:34:52 +0530

    Add invite creation event handler

--- src/events/inviteCreate.js
+++ src/events/inviteCreate.js
@@ -1,1 +1,15 @@
+import { Events } from 'discord.js';
+import { upsertInvite } from '../utils/inviteTracker.js';
 
+export default {
+  name: Events.InviteCreate,
+  once: false,
+
+  async execute(invite) {
+    try {
+      upsertInvite(invite);
+    } catch {
+      // ignore
+    }
+  },
+};
